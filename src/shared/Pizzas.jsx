7f1bf013b290598704e758bcce157a4737386f4a
victorHtoled <victor.game3107@gmail.com>
2024-08-09 09:31:50 -0300

    pizzas e mais pizzas

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,43 @@
+import Pepperoni from "../assets/images/pepperoni.png"
+import Frango from "../assets/images/Frango.png"
+import Hawaiana from "../assets/images/Hawaiana.png"
+import Margherita from "../assets/images/Margherita.png"
+import Calabresa from "../assets/images/Calabresa.png"
+
+
+const Pizzas =[
+    {
+        "id": 1,
+        "nome": "Margherita",
+        "descrição": "Molho de tomate, mussarela, manjericão fresco e azeite.",
+        "imagem" : Margherita
+    },
+    {
+        "id": 2,
+        "nome": "Pepperoni",
+        "descrição": "Molho de tomate, mussarela e fatias de pepperoni.",
+        "imagem" : Pepperoni
+    },
+    {
+        "id": 3,
+        "nome": "Frango",
+        "descrição": "Molho de tomate, mussarela, parmesão, gorgonzola e provolone.",
+        "imagem" : Frango
+    },
+    {
+        "id": 4,
+        "nome": "Calabresa",
+        "descrição": "Molho de tomate, mussarela, calabresa e cebola.",
+        "imagem" : Calabresa
+    },
+    {
+        "id": 5,
+        "nome": "Hawaiana",
+        "descrição": "Molho de tomate, mussarela, frango desfiado e catupiry.",
+        "imagem" : Hawaiana
+    },
+    
+    
+]
+
+export default Pizzas;
